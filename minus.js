96427150c47fc216ea8328098a32e4b051cee856
Viktória Hajduné Tamás <tmsvktr@gmail.com>
2023-07-19 15:16:00 +0200

    Minus.js modofied

--- minus.js
+++ minus.js
@@ -8,7 +8,7 @@ let checkButton = document.querySelector("#check")
 let newButton = document.querySelector("#new")
 let resultForm = document.querySelector("#resultForm")
 
-
+//blablabla dummy comments
 let hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 let hexColor = "#";
 
